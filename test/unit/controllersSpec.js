0f6c0aa4e56905434fa366872d100f0cdad56985
Kim Brewer <kabrewer07@gmail.com>
2014-09-29 22:00:22 -0400

    add test for new property

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -13,6 +13,7 @@ describe('controllers', function() {
           ctrl = $controller('PhoneListCtrl', {$scope:scope}); //We use $controller to create an instance of the PhoneListCtrl
 
       expect(scope.phones.length).toBe(3); //With this instance, we verify that the phones array property on the scope contains three records.
+      expect(scope.name).toBe('World');
     }));
 
   });
