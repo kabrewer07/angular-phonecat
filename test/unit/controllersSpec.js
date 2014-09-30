9e4f8a35956c76c916ee047f1fe36650fd34f17e
Kim Brewer <kabrewer07@gmail.com>
2014-09-29 23:05:45 -0400

    update controller test and add dropdown option

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -6,16 +6,36 @@
 describe('controllers', function() {
 
   describe('PhoneListCtrl', function(){
+    var scope, ctrl;
 
     beforeEach(module('phonecatApp')); //Before each test we tell Angular to load the phonecatApp module.
 
-    it('should create "phones" model with 3 phones', inject(function($controller) { //We ask Angular to inject the $controller service into our test function
-      var scope = {},
-          ctrl = $controller('PhoneListCtrl', {$scope:scope}); //We use $controller to create an instance of the PhoneListCtrl
-
-      expect(scope.phones.length).toBe(3); //With this instance, we verify that the phones array property on the scope contains three records.
-      expect(scope.name).toBe('World');
+    beforeEach(inject(function($controller) {
+      scope = {};
+      ctrl = $controller('PhoneListCtrl', {$scope:scope}); //We use $controller to create an instance of the PhoneListCtrl
     }));
+
+    it('should create "phones" model with 3 phones', function() {
+      expect(scope.phones.length).toBe(3); //With this instance, we verify that the phones array property on the scope contains three records.
+    });
+
+
+    it('should set the default value of orderProp model', function() {
+      expect(scope.orderProp).toBe('age');
+    });
+
+    // beforeEach(inject(function($controller) {
+    //   scope = {};
+    //   ctrl = $controller('PhoneListCtrl', {$scope:scope});
+    // }));
+
+    // it('should create "phones" model with 3 phones', inject(function($controller) { //We ask Angular to inject the $controller service into our test function
+    //   var scope = {},
+    //       ctrl = $controller('PhoneListCtrl', {$scope:scope}); //We use $controller to create an instance of the PhoneListCtrl
+
+    //   expect(scope.phones.length).toBe(3); //With this instance, we verify that the phones array property on the scope contains three records.
+      
+    // }));
 
   });
 
