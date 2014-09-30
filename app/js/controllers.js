b63856ad4e592e3cd42cea20cecdd76f14ffca76
Kim Brewer <kabrewer07@gmail.com>
2014-09-30 11:00:37 -0400

    restructure controller to fix minification issues

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,12 +3,25 @@
 /* Controllers */
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
-  $http.get('phones/phones.json').success(function(data) {
-    $scope.phones = data;
-  });
+// phonecatApp.controller('PhoneListCtrl', function ($scope, $http) { // to use a service, declare dependencies here - this looks up the dependency by name
+//   $http.get('phones/phones.json').success(function(data) {
+//     $scope.phones = data;
+//   });
 
-  $scope.orderProp = 'age';
-});
+//   $scope.orderProp = 'age';
+// });
+
+// var phonecatApp = angular.module('phonecatApp', []);
+
+//To fix minification issues structure like this:
+
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('phones/phones.json').success(function(data) {
+      $scope.phones = data;
+    });
+
+    $scope.orderProp = 'age';
+  }]);
 
 
